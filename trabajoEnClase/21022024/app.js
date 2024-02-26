4e10baa06eb2e2d1218621bc99ec2da120274785
Dany27HA <danny27ha@gmail.com>
2024-02-26 11:34:54 -0600

    Trabajo en clase 2602024

---
+++ trabajoEnClase/21022024/app.js
@@ -0,0 +1,28 @@
+//Imgresando Datos
+
+// let nombre= prompt("Ingresa tu nombre"); 
+// console.log(nombre);
+
+// let apellido= prompt("Ingresa tu apellido"); 
+// console.log(apellido);
+
+// let edad= prompt("Ingresa tu edad"); 
+// console.log(edad);
+
+// let numeroUno=prompt("Ingresa ek numero 1:");
+
+// let numeroDos=prompt("Ingresa ek numero 2:");
+
+// let resultado= numeroDos+numeroUno;
+
+// console.log(typeof resultado)
+// console.log(resultado);
+
+let numeroUno=prompt("Ingresa ek numero 1:");
+
+let numeroDos=prompt("Ingresa ek numero 2:");
+
+let resultado= parseInt (numeroUno)+ parseInt(numeroDos);
+
+console.log(typeof resultado)
+console.log(resultado);
---
+++ trabajoEnClase/26022024/app.js
@@ -0,0 +1,14 @@
+let datoUno=prompt("Ingresa Dato 1: ");
+let datoDos=prompt("Ingresa Dato 2: ");
+
+let suma= parseInt(datoUno)+ parseInt(datoDos);
+console.log("El resultado de la suma es: "+ suma);
+
+let resta= parseInt(datoUno)- parseInt(datoDos);
+console.log("El resultado de la resta es: "+ resta);
+
+let mul= parseInt(datoUno)* parseInt(datoDos);
+console.log("El resultado de la multiplicacion es: "+ mul);
+
+let div= parseInt(datoUno)/ parseInt(datoDos);
+console.log("El resultado de la division es: "+ div);
